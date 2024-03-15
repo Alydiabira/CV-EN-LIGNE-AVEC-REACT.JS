07e519b41ff272464c9a5c624bbc68f6cbff37ed
Aly <d-aly@outlook.fr>
2024-03-15 15:28:08 +0100

    Update formations.ts

--- src/data/formations.ts
+++ src/data/formations.ts
@@ -4,7 +4,11 @@ const formationsData = [
     date: "De mars 2023 à juillet 2023",
     text:"", 
     location: "La Grande Classe, 51 Rue Gaston Lauriau, 93100 Montreuil",
-    missions: ["Périodes militaire initiales/Découverte du milieu militaire ", "Participation à des activités militaires et sportives progressives et adaptées", "Développement du goût de l'effort", "Initiation aux techniques et connaissances de base du militaire", "Sensibilisation aux enjeux de la Défense Nationale"],
+    missions: ["analyser les besoins à partir du cahier des charges défini par le client ou le chef de projet. ",
+    "définir les différentes étapes de fonctionnement de l’application.", 
+    "étudier l’ensemble des solutions techniques disponibles", 
+    "assurer le support technique du site et produire une documentation technique et fonctionnelle du site pour le client.", 
+    "apporter d’éventuelles corrections de bugs et d’effectuer des mises à jour."],
 
   },
   {
