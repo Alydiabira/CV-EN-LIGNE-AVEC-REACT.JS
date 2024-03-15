8555e620516e7f26c440d6a06970cc2d9ba3d1c0
Aly <d-aly@outlook.fr>
2024-03-15 15:14:12 +0100

    Update experiences.ts

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -4,14 +4,14 @@ const experiencesData = [
     date: "Août 2019",
     location: "44e régiment de transmissions - MOLSHEIM",
     text: "Armée de terre.",
-    missions: ["mission 1", "mission 2", "mission 3"],
+    missions: ["Périodes militaire initiales/Découverte du milieu militaire ", "Participation à des activités militaires et sportives progressives et adaptées", "Développement du goût de l'effort", "Initiation aux techniques et connaissances de base du militaire", "Sensibilisation aux enjeux de la Défense Nationale"],
   },
   {
     title: "Développeur Web - Front-end",
     date: "12/05/2017 - 20/06/2018",
     location: "Angers",
     text: "Armée de terre.",
-    missions: ["mission 1", "mission 2", "mission 3"],
+    missions: ["mission", "mission", "mission"],
   },
   {
     title: "Développeur Web - Front-end",
