e806d9a30775e0dbc43021545cf7a8d13f47be20
Aly <d-aly@outlook.fr>
2024-03-15 15:25:01 +0100

    Update formations.ts

--- src/data/formations.ts
+++ src/data/formations.ts
@@ -1,18 +1,25 @@
 const formationsData = [
   {
-    title: "Licence Professionnelle Informatique Option Logiciels Libres",
-    date: "2012",
-    location: "ANGERS",
+    title: "Concepteur Développeur d'application",
+    date: "De mars 2023 à juillet 2023",
+    text:"", 
+    location: "La Grande Classe, 51 Rue Gaston Lauriau, 93100 Montreuil",
+    missions: ["Périodes militaire initiales/Découverte du milieu militaire ", "Participation à des activités militaires et sportives progressives et adaptées", "Développement du goût de l'effort", "Initiation aux techniques et connaissances de base du militaire", "Sensibilisation aux enjeux de la Défense Nationale"],
+
   },
   {
     title: "BTS Informatique de Gestion Option Développeur d'applications",
     date: "2007",
     location: "LAVAL",
+    missions: ["Périodes militaire initiales/Découverte du milieu militaire ", "Participation à des activités militaires et sportives progressives et adaptées", "Développement du goût de l'effort", "Initiation aux techniques et connaissances de base du militaire", "Sensibilisation aux enjeux de la Défense Nationale"],
+
   },
   {
     title: " Baccalauréat STT Informatique et Gestion",
     date: "2005",
     location: "LAVAL",
+    missions: ["Périodes militaire initiales/Découverte du milieu militaire ", "Participation à des activités militaires et sportives progressives et adaptées", "Développement du goût de l'effort", "Initiation aux techniques et connaissances de base du militaire", "Sensibilisation aux enjeux de la Défense Nationale"],
+
   },
 ];
 export default formationsData;
