2d0214dea0a21fbe5f13c502a513e911196f8c45
Aly <d-aly@outlook.fr>
2024-03-15 16:02:34 +0100

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,18 +6,21 @@ const Skills = () => {
   return (
     <>
       <div className="skills">
-        <h2 className="h2">Compétences</h2>
+        <h2 className="h2">Compétences & Connaissance</h2>
         <Skill title="HTML" rating={5} />
         <Skill title="CSS" rating={4} />
         <Skill title="JAVASCRIPT" rating={4} />
         <Skill title="REACT" rating={4} />
-        <Skill title="NODE" rating={3} />
+        <Skill title="BOOTSTRAP" rating={3} />
+        <Skill title="Python" rating={5} />
+        <Skill title="MSQL" rating={5} />
+
+        
       </div>
       <div className="skills">
         <h2 className="h2">Langues</h2>
-        <Skill title="Anglais" rating={4} />
-        <Skill title="Allemand" rating={2} />
-        <Skill title="Japonais" rating={1} />
+        <Skill title="Anglais" rating={2} />
+        <Skill title="Arabe" rating={2} />
       </div>
       <Interests />
     </>
