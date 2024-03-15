68a7427df36d171a7f8b18f2be632b7fb824a92e
Aly <d-aly@outlook.fr>
2024-03-15 15:03:49 +0100

    Create FormationsExperiences.tsx

---
+++ src/components/FormationsExperiences.tsx
@@ -0,0 +1,14 @@
+import "./FormationsExperiences.css";
+import Formations from "./Formations";
+import Experiences from "./Experiences";
+
+const FormationsExperiences = () => {
+  return (
+    <>
+      <Formations />
+      <Experiences />
+    </>
+  );
+};
+
+export default FormationsExperiences;
